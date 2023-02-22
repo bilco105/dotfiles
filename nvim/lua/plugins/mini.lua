c79be9564d9eaa8333c77487689c8b7f701ce1e2
Rob Greenwood <bilco105@gmail.com>
2023-02-22 15:24:29 +0000

    feat(nvim): give mini.indentscope a whirl

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -20,4 +20,15 @@ return {
       require("mini.surround").setup(opts)
     end,
   },
+  {
+    "echasnovski/mini.indentscope",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      symbol = "│",
+      options = { try_as_border = true },
+    },
+    config = function(_, opts)
+      require("mini.indentscope").setup(opts)
+    end,
+  },
 }
