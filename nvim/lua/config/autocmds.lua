0409ff3f41582dd7fc568c53cffd94c0b40dd13c
Rob Greenwood <bilco105@gmail.com>
2023-04-14 21:52:08 +0100

    feat(neovim): Add a couple of useful autocmds
    * Automatically create directories on save if the full path doesn't exist
    * Don't automatically continuing commenting new lines when entering insert mode

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,8 +1,28 @@
--- Highlight yank'd text after yankin'
-vim.api.nvim_create_augroup("YankHighlight", {})
+local function augroup(name)
+  return vim.api.nvim_create_augroup(name, { clear = true })
+end
+
+-- Don't continue comments when entering insert mode
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    vim.opt.formatoptions:remove { "o" }
+  end,
+  group = augroup "highlight_yank",
+  desc = "Disable New Line Comment",
+})
+
+-- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-  group = "YankHighlight",
   callback = function()
     vim.highlight.on_yank()
+  end,
+})
+
+-- Auto create dir when saving a file, in case some intermediate directory does not exist
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  group = augroup "auto_create_dir",
+  callback = function(event)
+    local file = vim.loop.fs_realpath(event.match) or event.match
+    vim.fn.mkdir(vim.fn.fnamemodify(file, ":p:h"), "p")
   end,
 })
